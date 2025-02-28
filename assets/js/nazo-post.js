3c8e995d4df2a68d5ba3ba57e655c8731064e567
kisasage <tanan2-o-univ.kisasage@outlook.jp>
2025-02-28 21:24:25 +0900

    update:回答フォーム追加

--- assets/js/nazo-post.js
+++ assets/js/nazo-post.js
@@ -1,3 +1,13 @@
 $(function() {
-    // console.log('ok');
+    $('#answerSubmitBtn').on('click', function() {
+        const answer = $('.answer').data('answer');
+        const userInput = $('#userAnswerInput').val();
+
+        $('.result').hide();
+        if (answer === userInput) {
+            $('#answerCorrect').show();
+        } else {
+            $('#answerIncorrect').show();
+        }
+    })
 })
